aa09ac05c4c9789630fa0dd8946f641a0f1f338e
Alexander Sova <bird@codeminders.com>
2020-09-19 22:58:13 +0000

    closes #7

--- intents/fallback.js
+++ intents/fallback.js
@@ -2,6 +2,6 @@ const dialogflow = require('../dialogflow');
 
 module.exports = async() => {
     dialogflow.intent('Default Fallback Intent', async(conv, params) => {
-        conv.ask('Sorry, what?');
+        conv.ask('Sorry, I did not get that. You can ask me to update your location or change the pollution type settings.');
     });
 }
