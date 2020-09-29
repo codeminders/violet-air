25fc97d88d19989705e7f0e35afdbd097a1de287
Alexander Sova <bird@codeminders.com>
2020-09-29 05:51:38 +0000

    wording for location helper fixed in accordance to Google review comments

--- request-location.js
+++ request-location.js
@@ -1,9 +1,16 @@
 const df = require('actions-on-google');
 
 module.exports = async(conv) => {
+    let context = '';
+    if (conv.user.storage.hasOwnProperty('returned_user')) 
+        context = 'To find out where you are';
+    else
+        context = 'Welcome to Purple Turtle. I can provide Real-time Air Quality Information for your location if you are not too far from PurpleAir sensors.';
+
     const options = {
-        context: 'To find out where you are',
+        context: context,
         permissions: ['DEVICE_PRECISE_LOCATION'],
     };
+    conv.user.storage.returned_user = true;
     return await conv.ask(new df.Permission(options));
 }
